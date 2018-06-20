f18052f34e7a9d1749943653c3b5891e1a63b83a
GitRekton <becker.marco@outlook.de>
2018-06-20 08:28:33 +0000

    added Drive.py

---
+++ drive.py
@@ -0,0 +1,12 @@
+import pigpio
+import argparse
+
+speed = 0
+
+parser = argparse.ArgumentParser()
+parser.add_argument("speed", help="adjust the car speed",type=int)
+
+args = parser.parse_args()
+
+pi = pigpio.pi()
+pi.set_PWM_dutycycle(12, args.speed)
